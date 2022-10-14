f736f6c273212582fa64a2291ff27fc75f4d1e20
Tariq A <tachorzyad@gmail.com>
2022-10-13 19:59:30 -0500

    minor edits

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 import array as a
 
-def findMax(arr, n, k):
+def findMax(arr, n, k = 0):
     if(n < 2): 
         return 0
     if(n == 2):
         return arr[1] - arr[0]
     max_num = 0
     for i in range(k, len(arr)):
-        res = arr[i] - arr[k]
-        if (res > max_num):
-            max_num = res
+        diff = arr[i] - arr[k]
+        if (diff > max_num):
+            max_num = diff
     print('current max is', max_num)
     return max(max_num, findMax(arr, n-1, k+1))
 
 def main():
     n = int(input())
     arr = a.array("i", [int(x) for x in input().split()])
-    print(findMax(arr, n, 0))
+    print(findMax(arr, n))
 
 if __name__ == "__main__":
     main()
